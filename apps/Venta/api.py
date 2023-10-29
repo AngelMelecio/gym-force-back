21680d040099ecd908e7d10cd26f3b5e1277e7c4
AngelMelecio <joseangelsm50@gmail.com>
2023-10-29 11:19:56 -0600

    api Venta

---
+++ apps/Venta/api.py
@@ -0,0 +1,27 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.decorators import parser_classes
+from rest_framework.parsers import MultiPartParser, JSONParser
+
+@api_view(['GET','POST'])
+@parser_classes([MultiPartParser , JSONParser])
+def venta_api_view(request):
+    if request.method == 'POST':
+        productos = request.data.get('productos', [])
+        suscripcion = request.data.get('suscripcion', None)
+
+        return Response({"message":"Todo bien"}, status=status.HTTP_200_OK)
+
+    if(request.method == 'GET'):
+        pass
+
+@api_view(['GET','PUT','DELETE'])
+@parser_classes([MultiPartParser, JSONParser])
+def venta_detail_api_view(request, pk=None ):
+    if request.method == 'GET':
+        pass
+    if request.method == 'PUT':
+        pass
+    if request.method == 'DELETE':
+        pass
